8f0dfd5be6559fba22eea4f918804e7520bb1140
jardimrafaela <62883934+jardimrafaela@users.noreply.github.com>
2025-08-24 15:10:41 -0300

    Update App.test.js

--- codebase/rdicidr-0.1.0/src/App.test.js
+++ codebase/rdicidr-0.1.0/src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/CIDR Calculator/i);
+  const linkElement = screen.getByText(/something else/i);
   expect(linkElement).toBeInTheDocument();
 });
